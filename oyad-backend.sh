4ddd53033432b6e7f346df8ef5884eac83da907a
vahit <vahid.maani@gmail.com>
2017-05-04 00:48:41 +0430

    vertion 0.1: single file -> OK

---
+++ oyad-backend.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+mplayer_args=$*
+
+mplayer=$(which mplayer)
+
+function volume_manager() {
+    # STEP ONE
+    for count in {0..24}; do
+	amixer sset Master 1%+ 1>/dev/null 2>&1
+	sleep 3s
+    done
+    
+    # wait 5 minutes until start step two
+    sleep 5m
+    
+    # STEP TWO
+    for count in {0..14}; do
+	amixer sset Master 2%+ 1>/dev/null 2>&1
+	sleep 5s
+    done
+    
+    # wait 5 minutes until start step three
+    sleep 10m
+    
+    # STEP THREE
+    for count in {0..6}; do
+	amixer sset Master 5%+ 1>/dev/null 2>&1
+	sleep 10s
+    done
+}
+
+# decrease the volume level to 10%
+amixer sset Master 10% 1>/dev/null 2>&1
+
+$mplayer $mplayer_args 1>/dev/null 2>&1 &
+volume_manager 1>/dev/null 2>&1
+
---
+++ oyad-frontend.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function sleep_time(){
+    alarm_time=$1
+    alarm_time_sec=$(date --date=$alarm_time +"%s")
+    current_time_sec=$(date +"%s")
+    if [[ $alarm_time_sec -lt $current_time_sec ]]; then
+	alarm_time_sec=$(date --date="tomorrow $alarm_time" +"%s")
+    fi
+    sleep_sec=$(($alarm_time_sec - $current_time_sec))
+    echo $sleep_sec
+}
+
+function single_file() {
+    song_file=$1
+    command="-loop 0 $song_file"
+}
+
+case $2 in
+    single) single_file $3;;
+    playlist) ;;
+    random) ;;
+    *) ;;
+esac
+
+sleep $(sleep_time $1)
+bash -x ./oyad-backend.sh $command
+
