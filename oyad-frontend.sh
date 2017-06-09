0fb03b7952f0c7c17a352c98d8cd1c1cd72dccf0
vahit <vahid.maani@gmail.com>
2017-06-09 19:20:51 +0430

    version2.0-Beta

--- oyad-frontend.sh
+++ oyad-frontend.sh
@@ -26,67 +26,41 @@ function tomorrow() {
     year=$(echo $today | cut -d"-" -f1)
     month=$(echo $today | cut -d"-" -f2)
     day=$(echo $today | cut -d"-" -f3)
-    day=$(($day + 1))
+    day=$(( 10#$day + 1 ))
     date --date="$year-$month-$day" > /dev/null 2>&1
     if [[ $? -eq 0 ]]; then
 	echo "$year-$month-$day"
 	return 0
     else
 	day=1
-	month=$(( $month + 1 ))
+	month=$(( 10#$month + 1 ))
 	date --date="$year-$month-$day" > /dev/null 2>&1
 	if [[ $? -eq 0 ]]; then
 	    echo "$year-$month-$day"
 	    return 0
 	else
 	    month=1
-	    year=$(( $year + 1 ))
+	    year=$(( 10#$year + 1 ))
 	fi
     fi
     echo "$year-$month-$day"
     return 0
 }
 
-function single_file() {
-    song_file=$1
-    command="-loop 0 $song_file"
-}
-
-function dir_files() {
-    song_files=$()
-}
-
-function playlist() {
-    playlist=$1
-    command="-playlist $playlist -loop 0"
-}
-
 function playlist_generator() {
-    musics_dir=$1
+    local musics_dir=$1
     for each_file in $(ls $musics_dir); do
-	if [[ $(file --brief $each_file | awk '{print $1}') == "directory" ]]; then
+	if [[ $(ls -lhd $musics_dir/$each_file | cut -c1) == "d" ]]; then
 	    playlist_generator "$musics_dir/$each_file"
 	else
 	    echo $musics_dir/$each_file >> /tmp/playlist.txt
 	fi
     done
-    playlist /tmp/playlist.txt
 }
 
 function nop() {
     echo "A problem occured. Please check inputs and options."
     exit 1
-}
-
-function source_detector() {
-    source=$1
-    source_type=$(file --brief $source | cut -f1 -d " ")
-    case $source_type in
-	Audio) single_file $source ;;
-	ASCII) playlist $source ;;
-	directory) playlist_generator $source ;;
-	*) nop ;;
-    esac
 }
 
 oyad_path=$(echo $0 | rev | cut -d"/" -f2- | rev)
@@ -100,7 +74,9 @@ else
     alarm_time="$correct_date $1"
 fi
 
-source_detector $2
+rm /tmp/playlist.txt
+playlist_generator $2
+command="-shuffle -playlist /tmp/playlist.txt -loop 0"
 echo "Alarmed at $alarm_time ..."
 sleep $remain_sec
 bash -x $oyad_path/oyad-backend.sh $command &
